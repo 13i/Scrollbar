c399dd4024e45b3df64b9e72034159535131c2af
13i <pierre@13i.fr>
2011-11-06 16:14:54 +0000

    mend

--- Source/scrollbar.js
+++ Source/scrollbar.js
@@ -3,57 +3,58 @@ var Scrollbar = new Class({
     Implements: Options,
     options: {
         // Number of pixels the contents moves up adns down
-		step: 10,
+    	step: 10,
         // Interval between 2 moves
 		inrterval: 100
 	},
 	element: null,
+	elements: {},
     sizes: {},
     slider: null,
     top: 0,
+    contentFx: null,
 	initialize: function(element, options){
+		element = document.id(element);
 		if(!element) return;
 		this.setOptions(options);
 		this.element = element;
 		this.element.store('MooScroll', this);
-		this.setup();
-        return this;
+        return this.setup();
 	},
-	setup: function(){
-        this.setupHTML().setupSizes().setupEvents();
-		return this;
-	},
-    setupHTML: function(){
+    setup: function(){
+    	
+    	var o = this.options;
         
-    	this.element.setStyles({'overflow': 'hidden', 'position': 'relative'});
-		
-        // Retrieve content
+        // Store original values
+        var originalPadding = this.element.getStyle('padding');
 		var html = this.element.get('html');
-		this.element.empty();
 		
-        // Create scrollbar layout
-		this.scroller = new Element('div', { 'class': 'Scrollbar-scroller' });
-		this.scroller.tarrow = new Element('div', {'class': 'Scrollbar-tArrow'}).inject(this.scroller);
-        this.scroller.scrollbar = new Element('div', {'class': 'Scrollbar-scrollbar'}).inject(this.scroller);
-        this.scroller.scrollbarInner = new Element('div', {'class': 'Scrollbar-scrollbarInner'}).inject(this.scroller.scrollbar);
-		this.scroller.button = new Element('div', {'class': 'Scrollbar-button'}).inject(this.scroller.scrollbarInner);
-		this.scroller.barrow = new Element('div', {'class': 'Scrollbar-bArrow'}).inject(this.scroller);
-		this.scroller.inject(this.element);
-        
-        // Create content layout
-		this.content = new Element('div', {'class': 'Scrollbar-content'});
-		this.content.fx = new Fx.Tween(this.content, { property: 'top', link: 'cancel', duration: 'short' });
-		new Element('div', {'class' : 'Scrollbar-contentInner', html: html}).inject(this.content);
-		this.content.inject(this.element);
-        
-        return this;
-    },
-    setupEvents: function(){
-        var timer,
-            o = this.options,
-            s = this.sizes;
-        
-        this.scroller.barrow.addEvents({
+    	this.element.setStyles({ overflow: 'hidden', position: 'relative', padding: 0}).empty();
+		
+        // Build elements
+		var elts = {};
+		['scroller', 'tArrow', 'scrollbar', 'scrollbarInner', 'button', 'bArrow', 'content', 'contentInner'].each(function(element, i){
+			elts[element] = new Element('div', { 'class': 'Scrollbar-'+element });
+		});
+		elts.tArrow.inject(elts.scroller);
+        elts.scrollbar.inject(elts.scroller);
+        elts.scrollbarInner.inject(elts.scrollbar);
+		elts.button.inject(elts.scrollbarInner);
+		elts.bArrow.inject(elts.scroller);
+		elts.scroller.inject(this.element);
+		elts.contentInner.set('html', html).setStyle('padding', originalPadding).inject(elts.content);
+		elts.content.inject(this.element);
+		this.elements = elts;
+		
+		// Scroll fx
+		this.contentFx = new Fx.Tween(elts.content, { property: 'top', link: 'cancel', duration: 'short' });
+		
+		// Compute initial sizes
+		this.update();
+		var s = this.sizes;
+		
+        // Add events to arrows
+        elts.bArrow.addEvents({
 			'mousedown': function(e){
 				e.preventDefault();
 				timer = this.moveOf.periodical(o.interval, this, [-o.step, true]);
@@ -63,8 +64,7 @@ var Scrollbar = new Class({
 				clearInterval(timer);
 			}.bind(this)
 		});
-		
-        this.scroller.tarrow.addEvents({
+        elts.tArrow.addEvents({
 			'mousedown': function(e){
 				e.preventDefault();
 				timer = this.moveOf.periodical(o.interval, this, [o.step, true]);
@@ -74,51 +74,60 @@ var Scrollbar = new Class({
 				clearInterval(timer);
 			}.bind(this)
 		});
-        
-        var drag = new Drag(this.scroller.button, {
+		
+        // Make button draggable
+        new Drag(elts.button, {
             limit: {
                 x: [0, 0],
-                y: [0, s.scrollBarInner - s.button]
+                y: [0, s.scrollbarInner - s.button]
             },
-            onDrag: function(el, e){
-                this.moveTo(- (el.getStyle('top').toInt() / s.scrollBarRatio));
+            onDrag: function(button, e){
+                this.moveTo(- (button.getStyle('top').toInt() / s.scrollbarRatio));
             }.bind(this)
         });
-        
-        /*
-        this.slider = new Slider(this.scroller.scrollbarInner, this.scroller.button, {
-            mode: 'vertical',
-            snap: true,
-            range: [0, this.sizes.scrollBarInner - this.sizes.button],
-            steps: this.sizes.scrollBarInner - this.sizes.button,
-            onChange: function(step){
-                this.moveTo(-step/this.sizes.ratio);
-            }.bind(this)
-        });
-        */
         
         return this;
     },
-    setupSizes: function(){
+    /**
+     * Move of (x) pixels
+     * x can be positive or negative
+     */
+    update: function(){
+    	var elts = this.elements;
         var s = {
-            content: this.content.getSize().y,
+            content: elts.content.getSize().y,
             element: this.element.getSize().y,
-            scrollbar: this.scroller.scrollbar.getSize().y,
-            tarrow: this.scroller.tarrow.getSize().y,
-            barrow: this.scroller.barrow.getSize().y
+            scrollbar: elts.scrollbar.getSize().y,
+            tArrow: elts.tArrow.getSize().y,
+            bArrow: elts.bArrow.getSize().y
         };
-        s.ratio = s.element / s.content;
-        s.scrollBarInner = s.scrollbar - (s.tarrow + s.barrow);
-        s.scrollBarRatio = s.scrollBarInner / s.content;
-        s.button = s.ratio*s.scrollBarInner;
+        s.scrollbarInner = s.scrollbar - (s.tArrow + s.bArrow);
+        s.scrollbarRatio = s.scrollbarInner / s.content;
+        s.button = (s.element / s.content)*s.scrollbarInner;
         s.maxTop = -(s.content - s.element);
         
-        this.scroller.scrollbar.setStyle('padding-top', s.tarrow);
-        this.scroller.scrollbarInner.setStyle('height', s.scrollBarInner);
-        this.scroller.button.setStyle('height', s.button);
+        elts.scrollbar.setStyle('padding-top', s.tArrow);
+        elts.scrollbarInner.setStyle('height', s.scrollbarInner);
+        elts.button.setStyle('height', s.button);
         
         this.sizes = s;
         
+        return this;
+    },
+    /**
+     * Move to (x) pixels
+     * x must be negative
+     */
+    moveTo: function(top, updateButtonPosition){
+        var sizes = this.sizes,
+        	elts = this.elements;
+    	top = [top, 0].pick().limit(sizes.maxTop, 0);
+        if(updateButtonPosition){
+            var buttonTop = -top*sizes.scrollbarRatio;
+            elts.button.setStyle('top', buttonTop);
+        }
+        elts.content.setStyle('top', top);
+        this.top = top.toInt();
         return this;
     },
     /**
@@ -129,23 +138,6 @@ var Scrollbar = new Class({
         var distance = [distance, 0].pick(),
             newTop = this.top + x;
         this.moveTo(newTop, updateButtonPosition);
-        return this;
-    },
-    /**
-     * Move to (x) pixels
-     * x must be negative
-     */
-    moveTo: function(top, updateButtonPosition){
-        console.log(top);
-        var sizes = this.sizes,
-            top = [top, 0].pick().limit(sizes.maxTop, 0);
-        if(updateButtonPosition){
-            var buttonTop = (-top*sizes.ratio).limit(0, sizes.scrollBarInner),
-                correction = (buttonTop*sizes.ratio).limit(0, sizes.button);
-                this.scroller.button.setStyle('top', buttonTop-correction);
-        }
-        this.content.setStyle('top', top);
-        this.top = top.toInt();
         return this;
     }
 });
